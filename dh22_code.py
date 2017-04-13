3e3b16f03a1f062bbbb15ea9072ab64fbda3e783
Nithin Jino <nj06606@uga.edu>
2017-04-12 21:49:18 -0400

    cleaned up logging code

--- dh22_code.py
+++ dh22_code.py
@@ -30,29 +30,28 @@ if isRoot:
 
     if humidity_1 is not None and temp_1 is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_1, humidity_1))
-	logToCSV(temp_1,humidity_1,1)
     else:
         print("can't read from Sensor 1")
         humidity_1,temp_1 = 99999
-        logToCSV(temp_1,humidity_1,1)
 
     if humidity_2 is not None and temp_2 is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_2, humidity_2))
-	logToCSV(temp_2,humidity_2,2)
     else:
         print("can't read from Sensor 2")
         humidity_2,temp_2 = 99999
-        logToCSV(temp_2,humidity_2,2)
 
     if humidity_3 is not None and temp_3 is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_3, humidity_3))
-        logToCSV(temp_3,humidity_3,3)
     else:
         print("can't read from Sensor 3")
         humidity_3,temp_3 = 99999
-	logToCSV(temp_3,humidity_3,3)
+	
+	 logToCSV(temp_1,humidity_1,1)
+	 logToCSV(temp_2,humidity_2,2)
+	 logToCSV(temp_3,humidity_3,3)
+	 
 
 else:
     print("run script as root")
-    
+
 
