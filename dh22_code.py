73933c80470eddeaf9fcacd6dca4bdaa4d1d1d91
Anton Franzluebbers <anton@uga.edu>
2017-04-14 14:43:09 -0400

    made number of sensors more configurable, and made the log file an absolute path for cron to work

--- dh22_code.py
+++ dh22_code.py
@@ -11,47 +11,39 @@ import time
 sensor_model = dht.DHT22
 
 #GPIO Pin that the sensor is attached to
-sensor_1 = 3
-sensor_2 = 15
-sensor_3 = 27
+sensor_pins = [3,15,27]
+
+num_sensors = 1
+
+#temp_1,humidity_1,temp_2,humidity_2,temp_3,humidity_3
 
 isRoot = os.getuid() == 0
 
 def logToCSV(temp, humidity, tag):
-    row = [time.time(), tag, temp, humidity]
-    with open('sensor_log.csv', 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow(row)
+	row = [time.time(), tag, temp, humidity]
+	with open('/home/sensors/sensors_finalproject/sensor_log.csv', 'a') as file:
+		writer = csv.writer(file)
+		writer.writerow(row)
 
 if isRoot:
-    humidity_1,temp_1 = dht.read(sensor_model,sensor_1)
-    humidity_2,temp_2 = dht.read(sensor_model,sensor_2)
-    humidity_3,temp_3 = dht.read(sensor_model,sensor_3)
+	humidity = []
+	temp = []
 
-    if humidity_1 is not None and temp_1 is not None:
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_1, humidity_1))
-    else:
-        print("can't read from Sensor 1")
-        humidity_1,temp_1 = 99999
+	for i in range(0,num_sensors):
+		h,t = dht.read_retry(sensor_model,sensor_pins[i])
+		humidity.append(h)
+		temp.append(t)
+		if humidity[i] is None:
+			print('Could not read humidity from sensor ' + i)
+			humidity[i] = 99999
+		if temp[i] is None:
+			temp[i] = 99999
+			print('Could not read temperature from sensor ' + i)
 
-    if humidity_2 is not None and temp_2 is not None:
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_2, humidity_2))
-    else:
-        print("can't read from Sensor 2")
-        humidity_2,temp_2 = 99999
-
-    if humidity_3 is not None and temp_3 is not None:
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp_3, humidity_3))
-    else:
-        print("can't read from Sensor 3")
-        humidity_3,temp_3 = 99999
-
-    logToCSV(temp_1,humidity_1,1)
-    logToCSV(temp_2,humidity_2,2)
-    logToCSV(temp_3,humidity_3,3)
-
+		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp[i], humidity[i]))
+		logToCSV(temp[i],humidity[i],i)
 
 else:
-    print("run script as root")
+	print("run script as root")
 
 
