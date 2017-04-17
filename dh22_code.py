a15ef18e106cd61f7be223297db619736059c66c
bunuble <bunuble@users.noreply.github.com>
2017-04-17 15:00:11 -0400

    Update dh22_code.py

--- dh22_code.py
+++ dh22_code.py
@@ -1,4 +1,6 @@
 import Adafruit_DHT as dht
+import RPi.GPIO as GPIO
+import time
 import os
 import csv
 import datetime
@@ -12,8 +14,15 @@ sensor_model = dht.DHT22
 
 #GPIO Pin that the sensor is attached to
 sensor_pins = [3,15,27]
-
 num_sensors = 2
+
+#ultrasonic sensor setup
+GPIO.setwarnings(False)			#suppresses GPIO warning
+GPIO.setmode(GPIO.BOARD)		#set GPIO pin numbering
+TRIG = 23				#associate pin 23 to TRIG
+ECHO = 24				#associate pin 24 to ECHO
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
 #temp_1,humidity_1,temp_2,humidity_2,temp_3,humidity_3
 
@@ -24,6 +33,37 @@ def logToCSV(temp, humidity, tag):
 	with open('/home/sensors/sensors_finalproject/sensor' + str(tag) + '_log.csv', 'a') as file:
 		writer = csv.writer(file)
 		writer.writerow(row)
+		
+def readUltraSonic():
+	counter = 1
+	data=[]
+
+	while (counter<11):
+		GPIO.output(TRIG, False)
+		time.sleep(.5)
+		GPIO.output(TRIG, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG, False)
+
+		while GPIO.input(ECHO)==0:
+			pulse_start = time.time()
+
+		while GPIO.input(ECHO)==1:
+			pulse_end = time.time()
+
+		pulse_duration = pulse_end - pulse_start
+		distance = pulse_duration*17150
+		distance = round(distance,2)
+		data.append(distance) #adds to the list
+		counter+=1
+
+	data.sort() #sorts the list
+	data = data[1:-1] #gets rid of the biggest and smallest number so it won't count any extremes
+	average = sum(data)/float(len(data)) #finds the average
+	average = round(average,2)
+
+	print(average)
+	return average;
 
 if isRoot:
 	humidity = []
