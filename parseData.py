20b5aec4f90ae22da2e4b0a3ae2549f8b58326c2
Nithin Jino <nj06606@uga.edu>
2017-04-26 18:37:46 -0400

    only accept proper humidity readings

--- parseData.py
+++ parseData.py
@@ -4,7 +4,12 @@ import os
 from datetime import datetime
 
 current_directory = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
-os.remove(current_directory + '/fixed_data.csv')
+
+try:
+    os.remove(current_directory + '/fixed_data.csv')
+except OSError:
+    pass
+
 with open('sensor0_log.csv','r') as sensor0, open('sensor1_log.csv','r') as sensor1:
     data0 = csv.reader(sensor0)
     data1 = csv.reader(sensor1)
@@ -16,10 +21,11 @@ with open('sensor0_log.csv','r') as sensor0, open('sensor1_log.csv','r') as sens
             date1 = datetime.strptime(row1[0][:-3],'%m/%d/%Y %H:%M')
 
             if date0 == date1:
-                fixed_row = [date0,row0[1],row0[2],row1[1],row1[2]]
-                with open(current_directory+'/fixed_data.csv','a') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(fixed_row)
-                count = count + 1
-                break;
+                if float(row0[2]) < 100.1 and float(row1[2]) < 100.1:
+                    fixed_row = [date0,row0[1],row0[2],row1[1],row1[2]]
+                    with open(current_directory+'/fixed_data.csv','a') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(fixed_row)
+                    count = count + 1
+                    break;
 print('Amount of matching dates:' + str(count))
